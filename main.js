956a895ac2e25496d26cbc7c40876b5745309789
shehab20089 <shehabmohsen1995@gmail.com>
2022-11-05 14:22:48 +0200

    create a function that gets the player choice

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   return possibleChoices[randomNumber];
 }
+
+// function to get the player choice
+function getPlayerChoice(text = "please enter your choice") {
+  const choice = prompt(text);
+  if (possibleChoices.includes(choice.toLowerCase())) {
+    return choice;
+  }
+  //force the user to choice between rock or paper or scissor
+  return getPlayerChoice(
+    "Please enter a valid user choice between rock , paper or scissor"
+  );
+}
