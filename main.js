6537425590c05050a37e0318dc02553fa8bae1d3
shehab20089 <shehabmohsen1995@gmail.com>
2022-11-05 14:23:29 +0200

    create 2 methods that play a round and check the strongest choice between 2 options

--- main.js
+++ main.js
@@ -17,3 +17,28 @@ function getPlayerChoice(text = "please enter your choice") {
     "Please enter a valid user choice between rock , paper or scissor"
   );
 }
+
+//function to play the round between the computer and the user
+function playRound(playerSelection, computerSelection) {
+  console.log(`player: ${playerSelection} , computer:${computerSelection}`);
+  if (playerSelection === computerSelection) console.log("draw");
+  else if (checkStrongerChoice(playerSelection, computerSelection))
+    console.log("Congratulations you win against the computer !!!");
+  else console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+}
+
+//helper function to check the strongest between two choices
+function checkStrongerChoice(choice1, choice2) {
+  if (choice1 === "rock") {
+    if (choice2 === "paper") return false;
+    return true;
+  }
+  if (choice1 === "paper") {
+    if (choice2 == "scissor") return false;
+    return true;
+  }
+  if (choice1 === "scissor") {
+    if (choice2 === "rock") return false;
+    return true;
+  }
+}
