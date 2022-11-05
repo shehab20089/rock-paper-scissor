4fb0eb551677148b07494c4a5a5f8688d7c6d15b
shehab20089 <shehabmohsen1995@gmail.com>
2022-11-05 14:22:19 +0200

    create a function to get the computer choice

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const possibleChoices = ["rock", "paper", "scissor"];
 
+// function to get the computer choice randomly
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  return possibleChoices[randomNumber];
+}
